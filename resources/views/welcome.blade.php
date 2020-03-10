a1150b59d781f8e6695308d571405981c6459c96
lismaya <lismayaok@gmail.com>
2020-03-10 12:25:21 +0700

    btsss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    BTS
                 </div>
 
                 <div class="links">
